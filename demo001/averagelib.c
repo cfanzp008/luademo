53eabdde6586f6540d0bdb123cbbe3f81fdfcb7a
cfanzp <cfan.zp@qq.com>
2014-05-09 14:26:33 +0800

    add demo001

---
+++ demo001/averagelib.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+extern "C"{
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+}
+
+lua_State* L;
+
+static int average(lua_State *L){
+    int n = lua_gettop(L);
+    double sum = 0;
+    int i;
+
+    for(int i = 1; i<=n;i++){
+        sum += lua_tonumber(L,i);
+    }
+
+    lua_pushnumber(L,sum/n);
+    lua_pushnumber(L,sum);
+
+    return 2;
+}
+
+int main(int argc,char *argv[]){
+    L = luaL_newstate();//lua_open();
+    luaL_openlibs(L);
+    lua_register(L,"average",average);
+    luaL_dofile(L,"avg.lua");
+    lua_close(L);
+
+    printf("Press enter to exit...");
+    getchar();
+
+    return 0;
+}
